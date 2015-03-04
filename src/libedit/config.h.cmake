967bdc332d65231fed2f0a37bf0e413643ae1008
Alfredo Kojima <alfredo.kengi.kojima@oracle.com>
2015-03-04 12:11:37 -0800

    Fixed build and linkage of libedit

---
+++ src/libedit/config.h.cmake
@@ -0,0 +1,18 @@
+
+#cmakedefine HAVE_TERM_H 1
+#cmakedefine HAVE_VIS_H 1
+#cmakedefine HAVE_STRVIS 1
+#cmakedefine HAVE_STRUNVIS 1
+#cmakedefine HAVE_STRLCPY 1
+#cmakedefine HAVE_STRLCAT 1
+#cmakedefine HAVE_FGETLN 1
+#cmakedefine HAVE_WCSDUP 1
+
+
+#include "sys.h"
+#ifndef NARROW_WRAPPER
+#define WIDECHAR
+#endif
+
+
+
