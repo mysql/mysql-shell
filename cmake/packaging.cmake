e018d9cf446f33f491a687a71cc93ae6e9c61f09
Rene Ramirez <j.rene.ramirez@oracle.com>
2015-03-04 12:36:44 -0600

    Pushing CMake patches sent by Kent - Adds version information - Correct lookup of V8 libraries - Addition of packaging makefile - Removal of shell FindMySQL to use the improved version in ngcommon
    Added patch to properly build Release version

---
+++ cmake/packaging.cmake
@@ -0,0 +1,123 @@
+# Copyright (c) 2015, Oracle and/or its affiliates. All rights reserved.
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+##############################################################################
+#
+#  Packaging
+#
+##############################################################################
+
+# Some stuff for building RPMs, that is not using CPack
+# RPMs doesn't allow "-" in version, so we use like "5.3.0_alpha"
+string(REPLACE "-" "_" MYSH_NODASH_VERSION "${MYSH_VERSION}")
+
+# The rest is mainly about CPack
+set(CPACK_PACKAGE_VERSION_MAJOR ${MYSH_MAJOR})
+set(CPACK_PACKAGE_VERSION_MINOR ${MYSH_MINOR})
+set(CPACK_PACKAGE_VERSION_PATCH ${MYSH_PATCH})
+
+if(NOT EXTRA_NAME_SUFFIX)
+  set(EXTRA_NAME_SUFFIX "")
+endif()
+if(NOT EXTRA_NAME_SUFFIX2)
+  set(EXTRA_NAME_SUFFIX2 "")
+endif()
+
+set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "MySQLng Shell ${MYSH_BASE_VERSION}, a library and tool for XXXXX")
+set(CPACK_PACKAGE_NAME                "mysql-ngshell${EXTRA_NAME_SUFFIX}")
+#if(EXISTS "${CMAKE_SOURCE_DIR}/COPYING")
+#  set(CPACK_RESOURCE_FILE_LICENSE     "${CMAKE_SOURCE_DIR}/COPYING")
+#else()
+#  set(CPACK_RESOURCE_FILE_LICENSE     "${CMAKE_SOURCE_DIR}/LICENSE.mysql")
+#endif()
+set(CPACK_PACKAGE_DESCRIPTION_FILE    "${CMAKE_SOURCE_DIR}/README")
+set(CPACK_SOURCE_PACKAGE_FILE_NAME    "${CPACK_PACKAGE_NAME}-${MYSH_VERSION}-src")
+set(CPACK_PACKAGE_INSTALL_DIRECTORY   "${CPACK_PACKAGE_NAME}-${MYSH_VERSION}${EXTRA_NAME_SUFFIX2}${PACKAGE_DRIVER_TYPE_SUFFIX}-${MYSH_PLATFORM}")
+set(CPACK_PACKAGE_FILE_NAME           "${CPACK_PACKAGE_INSTALL_DIRECTORY}")
+if(WIN32)
+  set(CPACK_GENERATOR                 "ZIP")
+else()
+  set(CPACK_GENERATOR                 "TGZ")
+endif()
+
+set(CPACK_SOURCE_IGNORE_FILES
+\\\\.git/
+\\\\.gitignore
+CMakeCache\\\\.txt
+CPackSourceConfig\\\\.cmake
+CPackConfig\\\\.cmake
+VersionInfo\\\\.h$
+postflight$
+/cmake_install\\\\.cmake
+/CTestTestfile\\\\.cmake
+/CMakeFiles/
+/_CPack_Packages/
+Makefile$
+cmake/sql.*\\\\.c$
+)
+
+#------------ Installation ---------------------------
+
+if(WIN32)
+
+# TODO: line-ending conversions unix->dos
+
+# install(FILES ChangeLog     DESTINATION . RENAME ChangeLog.txt)
+  install(FILES README        DESTINATION . RENAME README.txt)
+# install(FILES INSTALL       DESTINATION . RENAME INSTALL.txt)
+# if(EXISTS "${CMAKE_SOURCE_DIR}/COPYING")
+#   install(FILES COPYING       DESTINATION . RENAME COPYING.txt)
+# else()
+#   install(FILES LICENSE.mysql DESTINATION . RENAME LICENSE.mysql.txt)
+# endif()
+
+  # Install all .pdb files to enable debugging. Note that what build
+  # type and what sub directory the binaries ends up in, like
+  # "Release" and "Debug", is not determined until we run "devenv" or
+  # similar. So when running "cmake" we don't know the location. We
+  # can't test for the location here, a if(EXISTS ...) is run at
+  # "cmake" invocation time, not when we are to install. So we do a
+  # bit of a hack here until finding a better solution.
+  install(DIRECTORY
+    ${PROJECT_BINARY_DIR}/bin/RelWithDebInfo/
+    ${PROJECT_BINARY_DIR}/bin/Debug/
+    DESTINATION bin
+    FILES_MATCHING
+    PATTERN *.pdb
+  )
+  install(DIRECTORY
+    ${PROJECT_BINARY_DIR}/lib/RelWithDebInfo/
+    ${PROJECT_BINARY_DIR}/lib/Debug/
+    DESTINATION lib
+    FILES_MATCHING
+    PATTERN *.pdb
+  )
+
+else()
+
+# install(FILES ChangeLog    DESTINATION .)
+  install(FILES README       DESTINATION .)
+# install(FILES INSTALL      DESTINATION .)
+# if(EXISTS "${CMAKE_SOURCE_DIR}/COPYING")
+#   install(FILES COPYING       DESTINATION .)
+# else()
+#   install(FILES LICENSE.mysql DESTINATION .)
+# endif()
+
+endif()
+
+#install(FILES Licenses_for_Third-Party_Components.txt DESTINATION .)
+
+include(CPack)
