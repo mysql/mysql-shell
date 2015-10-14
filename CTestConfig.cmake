70f1a4fabd3df9fe4b87837edab0bc027810b336
Alfredo Kojima <alfredo.kengi.kojima@oracle.com>
2015-10-14 16:00:54 -0700

    Update ctest configs

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -10,6 +10,6 @@ set(CTEST_PROJECT_NAME "NGShell")
 set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
 
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "pandora03.se.oracle.com")
+set(CTEST_DROP_SITE "tyr06.no.oracle.com")
 set(CTEST_DROP_LOCATION "/cdash/submit.php?project=NGShell")
 set(CTEST_DROP_SITE_CDASH TRUE)
