f75ed0cffa237522f54c184b9e84b20d587e67f2
Ruben Morquecho <ruben.morquecho@oracle.com>
2016-01-28 08:36:58 -0800

    upload fixes for failing testcases

--- shell-qa-test/aux_files/UpdateView_NodeMode.py
+++ shell-qa-test/aux_files/UpdateView_NodeMode.py
@@ -1,3 +1,6 @@
-session.sql("USE sakila");
-session.sql("alter view actor_info as select count(actor_id) as ActorQuantity75 from actor;");
-session.sql("SELECT * FROM actor_info;");
+session.sql("USE sakila")
+session.sql("drop view if exists py_view;")
+session.sql("create view py_view as select first_name from actor where first_name like '%a%';")
+
+session.sql("alter view py_view as select count(actor_id) as ActorQuantity from actor;")
+session.sql("SELECT * FROM py_view ;")
