b5d4c0be034a9c3477ff5397ff551f40368f40c3
omendez <omar.mendez@oracle.com>
2016-02-25 11:02:54 -0600

    Added big data scenarios for 1000 rows, for Javascript and Python. Including the required files in aux_files folder

---
+++ shell-qa-test/aux_files/BigCreate_Coll_Node.py
@@ -0,0 +1,25 @@
+schema = session.getSchema('world_x')
+coll = schema.createCollection('big_coll_node_py')
+id_m = 1
+while id_m <= 1000:
+	res = {
+		"GNP": 414972,
+		"Extra_ID": str(id_m),
+		"Name": "Mexico", 
+		"IndepYear": 1810,
+		"geography": {
+			"Region": "Central America",
+			"Continent": "North America", 
+			"SurfaceArea": 1958201
+			}, 
+		"government": {
+			"HeadOfState": "Enrique Penia Nieto", 
+			"GovernmentForm": "Federal Republic"
+			}, 
+		"demographics": {
+			"Population": 98881000,
+			"LifeExpectancy": 71.5
+			}
+		}
+	coll.add(res).execute()
+	id_m = id_m + 1
