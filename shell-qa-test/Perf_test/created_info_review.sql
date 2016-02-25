58c0eb03e8d79ec7bd951c4809d69ac6dde916b0
omendez <omar.mendez@oracle.com>
2016-02-25 11:10:53 -0600

    Scripts and Python files used to execute a quick time measurement performance in local environment for Win 8 (be aware this is not ready to be executed by jenkins)

---
+++ shell-qa-test/Perf_test/created_info_review.sql
@@ -0,0 +1,8 @@
+SELECT count(*) FROM xshell_perf.perf_mysqlx_node_js;
+SELECT * FROM xshell_perf.perf_mysqlx_node_js limit 1;
+
+SELECT count(*) FROM xshell_perf.perf_mysqlx_classic_js;
+SELECT * FROM xshell_perf.perf_mysqlx_classic_js limit 1;
+
+SELECT count(*) FROM xshell_perf.perf_mysql_js;
+SELECT * FROM xshell_perf.perf_mysql_js limit 1;
