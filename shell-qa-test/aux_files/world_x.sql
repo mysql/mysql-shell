f0407718f6a33a92634c276c0de5a37374eef07e
Ruben Morquecho <ruben.morquecho@oracle.com>
2016-04-07 11:20:47 -0700

    updated to init schemas world_x and sakila

--- shell-qa-test/aux_files/world_x.sql
+++ shell-qa-test/aux_files/world_x.sql
@@ -15,11 +15,6 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-DROP SCHEMA IF EXISTS world_x;
-CREATE SCHEMA world_x;
-USE world_x;
-
-
 --
 -- Table structure for table `City`
 --
