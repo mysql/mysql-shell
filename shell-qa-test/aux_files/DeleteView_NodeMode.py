b36aa77ec86f176309d88f223b8ded83dd602e77
omendez <omar.mendez@oracle.com>
2016-08-01 12:27:38 -0500

    Continue with the test cases updates due to Python method name updates.

--- shell-qa-test/aux_files/DeleteView_NodeMode.py
+++ shell-qa-test/aux_files/DeleteView_NodeMode.py
@@ -1,5 +1,4 @@
 session.sql('drop view if exists py_view;').execute()
 session.sql("create view py_view as select first_name from actor where first_name like '%a%';").execute()
-session.getSchema('sakila').getViews()
-session.dropView('sakila','py_view')
+session.drop_view('sakila','py_view')
 session.sql("SELECT table_name FROM information_schema.views WHERE information_schema.views.table_schema LIKE '%py_view%';").execute()
