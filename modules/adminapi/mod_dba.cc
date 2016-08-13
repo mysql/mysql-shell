743ed9b63bd9899aacd1969f93c49d72d20dbbdf
Miguel Araujo <miguel.araujo@oracle.com>
2016-08-13 12:26:13 +0100

    Added output message for dropMetadataSchema()

--- modules/adminapi/mod_dba.cc
+++ modules/adminapi/mod_dba.cc
@@ -458,6 +458,8 @@ shcore::Value Dba::drop_metadata_schema(const shcore::Argument_list &args)
         _farms.reset(new shcore::Value::Map_type);
 
       _default_farm = "";
+
+      shcore::print("Metadata Schema successfully removed.\n").
     }
     CATCH_AND_TRANSLATE_FUNCTION_EXCEPTION(get_function_name("dropMetadataSchema"))
   }
