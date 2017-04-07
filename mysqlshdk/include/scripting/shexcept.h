e1b5d7182c72d822e6db56370750131df43350cc
Alfredo Kojima <alfredo.kengi.kojima@oracle.com>
2017-04-07 07:33:38 -0700

    Missing exception files

---
+++ mysqlshdk/include/scripting/shexcept.h
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2017, Oracle and/or its affiliates. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; version 2 of the
+ * License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301  USA
+ */
+
+#ifndef MYSQLSHDK_INCLUDE_SCRIPTING_SHEXCEPT_H_
+#define MYSQLSHDK_INCLUDE_SCRIPTING_SHEXCEPT_H_
+
+#include <string>
+#include <stdexcept>
+
+namespace shcore {
+
+/** External database errors
+ *
+ * Sample uses:
+ * - MySQL errors
+ */
+class database_error : public std::runtime_error {
+ public:
+  database_error(const char *what, int code, const char *error);
+
+  int code() const {
+    return code_;
+  }
+  const std::string &error() const {
+    return error_;
+  }
+
+ private:
+  int code_;
+  std::string error_;
+};
+
+/** Invalid input from the user
+ *
+ * Sample uses:
+ * - user input validation errors
+ */
+class user_input_error : public std::runtime_error {
+ public:
+  user_input_error(const char *what, const char *parameter);
+  explicit user_input_error(const char *what);
+
+ private:
+  std::string param_;
+};
+
+/** Operation cancelled by the user
+ *
+ * Sample uses:
+ * - User hit ^C
+ **/
+class cancelled : public std::runtime_error {
+ public:
+  explicit cancelled(const char *what);
+};
+
+/** Errors caused by an error in the user's script
+ *
+ * Sample uses:
+ * - Python or JS script/snippet executed by the user has some error in its
+ *  (like a syntax error or something)
+ *
+ * Usually scripting errors are supposed to be caught inside the script.
+ */
+class script_error : public std::runtime_error {
+ public:
+  explicit script_error(const char *what);
+};
+}  // namespace shcore
+
+#endif  // MYSQLSHDK_INCLUDE_SCRIPTING_SHEXCEPT_H_
---
+++ mysqlshdk/scripting/shexcept.cc
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2017, Oracle and/or its affiliates. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; version 2 of the
+ * License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301  USA
+ */
+
+#include "scripting/shexcept.h"
+
+namespace shcore {
+
+database_error::database_error(const char *what, int code, const char *error)
+    : std::runtime_error(what), code_(code), error_(error) {}
+
+user_input_error::user_input_error(const char *what, const char *parameter)
+    : std::runtime_error(what), param_(parameter) {}
+
+cancelled::cancelled(const char *what) : std::runtime_error(what) {}
+
+script_error::script_error(const char *what) : std::runtime_error(what) {}
+
+}  // namespace shcore
