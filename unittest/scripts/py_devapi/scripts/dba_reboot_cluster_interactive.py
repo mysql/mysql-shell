8f243b4664277e0174827c92e4d1a1b07c21ef92
Nelson Goncalves <nelson.goncalves@oracle.com>
2017-06-08 17:38:12 +0100

    BUG#25516390: REMOVE INSTANCES LIST DOESN'T VALIDATE THE LIST,PROMPTS USER FOR REJOIN/REMOVE
    This patch fixes an issue with the interactive layer of the
    rebootClusterFromCompleteOutage command.
    
    The interactive layer, when prompting for the instances to rejoin the
    cluster, was not ignoring the instances already specified using the
    removeInstances list. This made it possible for the same instance to
    be specified twice, once in the removeInstances list (manually
    specified by the user) and once in the rejoinInstances list
    (automatically added by the wizard of the interactive layer).
    
    With this patch, instances that are eligible to be added to the
    rejoinInstances list but that are specified in the removeInstances
    list are skipped by the wizard.
    
    This patch also adds validation on the non interactive layer for the
    rejoinInstances list that throws an error if unreachable instances are
    specified.

--- unittest/scripts/py_devapi/scripts/dba_reboot_cluster_interactive.py
+++ unittest/scripts/py_devapi/scripts/dba_reboot_cluster_interactive.py
@@ -79,6 +79,12 @@ session.close()
 # Re-establish the connection to instance 1
 shell.connect({'scheme': 'mysql', 'host': localhost, 'port': __mysql_sandbox_port1, 'user': 'root', 'password': 'root'})
 
+#@ Dba.rebootClusterFromCompleteOutage error unreachable server cannot be on the rejoinInstances list
+cluster = dba.reboot_cluster_from_complete_outage("dev", {'rejoinInstances': [localhost + ':' + str(__mysql_sandbox_port3)]})
+
+#@ Dba.rebootClusterFromCompleteOutage error cannot use same server on both rejoinInstances and removeInstances list
+cluster = dba.reboot_cluster_from_complete_outage("dev", {'rejoinInstances': [localhost + ':' + str(__mysql_sandbox_port2)], 'removeInstances': [localhost + ':' + str(__mysql_sandbox_port2)]})
+
 #@ Dba.rebootClusterFromCompleteOutage success
 cluster = dba.reboot_cluster_from_complete_outage("dev")
 
@@ -87,6 +93,73 @@ wait_slave_state(cluster, uri2, "ONLINE")
 
 #@<OUT> cluster status after reboot
 cluster.status()
+
+# Start instance 3
+if __sandbox_dir:
+  dba.start_sandbox_instance(__mysql_sandbox_port3, {'sandboxDir': __sandbox_dir})
+else:
+  dba.start_sandbox_instance(__mysql_sandbox_port3)
+
+#@ Add instance 3 back to the cluster
+add_instance_to_cluster(cluster, __mysql_sandbox_port3)
+
+# Waiting for the third added instance to become online
+wait_slave_state(cluster, uri3, "ONLINE")
+
+#@ Dba.rebootClusterFromCompleteOutage regression test for BUG#25516390
+# kill all the instances
+# Kill instance 2
+if __sandbox_dir:
+  dba.kill_sandbox_instance(__mysql_sandbox_port2, {'sandboxDir': __sandbox_dir})
+else:
+  dba.kill_sandbox_instance(__mysql_sandbox_port2)
+
+# Since the cluster has quorum, the instance will be kicked off the
+# Cluster going OFFLINE->UNREACHABLE->(MISSING)
+wait_slave_state(cluster, uri2, "(MISSING)")
+
+# Kill instance 3
+if __sandbox_dir:
+  dba.kill_sandbox_instance(__mysql_sandbox_port3, {'sandboxDir': __sandbox_dir})
+else:
+  dba.kill_sandbox_instance(__mysql_sandbox_port3)
+
+# Waiting for the third added instance to become unreachable
+# Will remain unreachable since there's no quorum to kick it off
+wait_slave_state(cluster, uri3, "UNREACHABLE")
+session.close()
+
+# Kill instance 1
+if __sandbox_dir:
+  dba.kill_sandbox_instance(__mysql_sandbox_port1, {'sandboxDir': __sandbox_dir})
+else:
+  dba.kill_sandbox_instance(__mysql_sandbox_port1)
+
+# Re-start all the instances
+
+# Start instance 2
+if __sandbox_dir:
+  dba.start_sandbox_instance(__mysql_sandbox_port2, {'sandboxDir': __sandbox_dir})
+else:
+  dba.start_sandbox_instance(__mysql_sandbox_port2)
+
+# Start instance 1
+if __sandbox_dir:
+  dba.start_sandbox_instance(__mysql_sandbox_port1, {'sandboxDir': __sandbox_dir})
+else:
+  dba.start_sandbox_instance(__mysql_sandbox_port1)
+
+# Start instance 3
+if __sandbox_dir:
+  dba.start_sandbox_instance(__mysql_sandbox_port3, {'sandboxDir': __sandbox_dir})
+else:
+  dba.start_sandbox_instance(__mysql_sandbox_port3)
+
+# Re-establish the connection to instance 1
+shell.connect({'scheme': 'mysql', 'host': localhost, 'port': __mysql_sandbox_port1, 'user': 'root', 'password': 'root'})
+
+cluster = dba.reboot_cluster_from_complete_outage("dev", {'removeInstances': [localhost + ':' + str(__mysql_sandbox_port2), localhost + ':' + str(__mysql_sandbox_port3)]})
+
 session.close()
 
 #@ Finalization
