adca4a42656c296746464756712d7189fb560747
Konrad Olesinski <konrad.olesinski@oracle.com>
2017-07-24 10:33:03 +0200

    Bug#25238576 - wrap logger instance in a unique_ptr so it will be dealocated when shell exits

--- common/logger/logger.cc
+++ common/logger/logger.cc
@@ -69,7 +69,7 @@
 using namespace ngcommon;
 
 
-Logger* Logger::instance = NULL;
+std::unique_ptr<Logger> Logger::instance(nullptr);
 
 Logger::Logger_levels_table Logger::log_levels_table;
 
@@ -114,7 +114,7 @@ Logger::LOG_LEVEL Logger::get_log_level()
 
 void Logger::assert_logger_initialized()
 {
-  if (instance == NULL)
+  if (instance.get() == nullptr)
   {
     const char *msg_noinit = "Logger: Tried to log to an uninitialized logger.\n";
 #ifdef WIN32
@@ -153,7 +153,7 @@ void Logger::log_text(LOG_LEVEL level, const char* domain, const char* text)
 {
   assert_logger_initialized();
 
-  if (instance && level <= instance->log_level)
+  if (instance.get() != nullptr && level <= instance->log_level)
   {
     std::string s = instance->format_message(domain, text, level);
     s += "\n";
@@ -178,7 +178,7 @@ void Logger::log(LOG_LEVEL level, const char* domain, const char* formats, ...)
 {
   assert_logger_initialized();
 
-  if (instance && level <= instance->log_level)
+  if (instance.get() != nullptr && level <= instance->log_level)
   {
     char *mybuf;
     va_list args;
@@ -218,7 +218,7 @@ void Logger::log(LOG_LEVEL level, const char* domain, const char* formats, ...)
 void Logger::log_exc(const char *domain, const char* message, const std::exception &exc)
 {
   assert_logger_initialized();
-  if (instance)
+  if (instance.get() != nullptr)
   {
     Logger::LOG_LEVEL level = Logger::LOG_ERROR;
     std::string s = instance->format_message(domain, message, exc);
@@ -244,9 +244,9 @@ void Logger::log_exc(const char *domain, const char* message, const std::excepti
 
 Logger* Logger::singleton()
 {
-  if (instance)
+  if (instance.get() != nullptr)
   {
-    return instance;
+    return instance.get();
   }
   else
   {
@@ -315,7 +315,7 @@ void Logger::out_to_stderr(const char* msg) {
 
 void Logger::setup_instance(const char* filename, bool use_stderr,
                             Logger::LOG_LEVEL log_level) {
-  if (instance) {
+  if (instance.get() != nullptr) {
     if (filename && instance->out_name.compare(filename) != 0) {
       instance->out.close();
       instance->out.open(filename, std::ios_base::app);
@@ -327,7 +327,7 @@ void Logger::setup_instance(const char* filename, bool use_stderr,
     instance->set_log_level(log_level);
     instance->use_stderr = use_stderr;
   } else {
-    instance = new Logger(filename, use_stderr, log_level);
+    instance.reset(new Logger(filename, use_stderr, log_level));
   }
 }
 
