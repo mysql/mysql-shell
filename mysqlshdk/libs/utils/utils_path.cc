372429e9fefcf0971c1a98b4211aa245d31e8364
Krzysztof Grzadziel <krzysztof.grzadziel@oracle.com>
2017-09-21 14:26:43 +0200

    BUG#25676405: PATH EXPANSION IS NOT SUPPORTED ON THE SHELL
    Add support for tilde expansion. If path begins with `~`, all of the
    characters up to first OS specific separator are considered as
    a tilde-prefix. All characters after `~` are considered as `loginname`.
    
    If `loginname` is empty then:
      On Unix: `HOME` environment variable is returned if is set, otherwise
      current user home directory is looked up in password directory.
    
      On Windows: `HOME` or `USERPROFILE` or `HOMEDRIVE`+`HOMEPATH`
      environment variable is returned if is set, otherwise user home
      directory is obtained using `FOLDERID_Profile` `KNOWNFOLDERID`.
      If all of above fail, empty string is returned.
    
    If `loginname` is not empty then:
      On Unix: User home directory path associated with `loginname` is
      looked up directly in the password directory.
    
      On Windows: Retrieving home directory of another user on Windows
      platform is *NOT SUPPORTED*.
    
    If the expansion fails or if the path does not begin with `~`, the
    path is returned unchanged.

---
+++ mysqlshdk/libs/utils/utils_path.cc
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2017, Oracle and/or its affiliates. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; version 2 of the
+ * License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301  USA
+ */
+
+#include "mysqlshdk/libs/utils/utils_path.h"
+
+#include <algorithm>
+#include <string>
+
+namespace shcore {
+namespace path {
+namespace detail {
+std::string expand_user(const std::string &path, const std::string &sep) {
+  if (path.size() > 0) {
+    if (path[0] == '~') {
+      std::string user_home;
+      auto p = std::find_first_of(begin(path), end(path), begin(sep), end(sep));
+      auto tilde_prefix_length = std::distance(begin(path), p);
+
+      if (tilde_prefix_length == 1) {
+        // `~` prefix
+        user_home = home();
+      } else if (tilde_prefix_length > 1) {
+        // `~loginname` prefix
+        auto login_name = std::string(begin(path) + 1, p);
+        user_home = home(login_name);
+      }
+
+      if (!user_home.empty()) {
+        return user_home + std::string(p, end(path));
+      }
+    }
+  }
+  return path;
+}
+}  // namespace detail
+}  // namespace path
+}  // namespace shcore
