b93b431c1bbf6b8b9c74b7796afa0d2f566dd0ed
Rene Ramirez <j.rene.ramirez@oracle.com>
2017-12-12 18:57:25 -0600

    Fixing mysqlx.Collection test

--- unittest/scripts/js_devapi/validation/mysqlx_collection.js
+++ unittest/scripts/js_devapi/validation/mysqlx_collection.js
@@ -110,7 +110,7 @@ SYNTAX
 |Query OK, 1 item affected|
 
 //@ addOrReplaceOne: error adding with key (BUG#27013165)
-|Query OK, 0 items affected|
+||Unable upsert data in document collection 'add_or_replace_one' (MySQL Error 5018)
 
 //@ addOrReplaceOne: error replacing with key
 ||Document contains a field value that is not unique but required to be
--- unittest/scripts/py_devapi/validation/mysqlx_collection.py
+++ unittest/scripts/py_devapi/validation/mysqlx_collection.py
@@ -113,7 +113,7 @@ SYNTAX
 |Query OK, 1 item affected|
 
 #@ add_or_replace_one: error adding with key (BUG#27013165)
-|Query OK, 0 items affected|
+||MySQL Error (5018): Unable upsert data in document collection 'add_or_replace_one'
 
 #@ add_or_replace_one: error replacing with key
 ||Document contains a field value that is not unique but required to be
