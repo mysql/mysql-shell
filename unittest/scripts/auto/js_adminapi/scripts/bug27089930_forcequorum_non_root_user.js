1c66b9849cfd28c664a598ec608894ee39ff767d
Paulo Jesus <paulo.jesus@oracle.com>
2018-01-15 12:51:28 +0000

    BUG#27089930 : forceQuorumUsingPartitionOf does not handle non root user
    The AdminAPI function forceQuorumUsingPartitionOf() does not wort with a
    non 'root' user, even if that user has all the required privileges. This
    happens because internally this operation is using the 'root' user to connect
    to other instances instead of the specified user.
    
    It is assumed that the same user (cluster administration user) with the same
    authentication credentials and privileges is available on all the instances,
    but that user should not be forced to be the 'root'. The user specified
    with the clusterAdmin option of the dba.configureLocalInstance() function
    is expected to be used as cluster administration user on all instance to
    execute any AdminAPI operation.
    
    This patch fixes that issue by using the specified user to connect to all
    instances, instead of the previously hard-coded 'root' user.
    
    Test was added for regression.

---
+++ unittest/scripts/auto/js_adminapi/scripts/bug27089930_forcequorum_non_root_user.js
@@ -0,0 +1,57 @@
+// Deploy instances.
+testutil.deploySandbox(__mysql_sandbox_port1, "root");
+testutil.deploySandbox(__mysql_sandbox_port2, "root");
+testutil.deploySandbox(__mysql_sandbox_port3, "root");
+
+//@ Configure instance 1 creating an Admin User.
+var cnfPath1 = testutil.getSandboxConfPath(__mysql_sandbox_port1);
+dba.configureLocalInstance(__sandbox_uri1, {mycnfPath: cnfPath1, clusterAdmin: "gr_user", clusterAdminPassword: "gr_pass"});
+
+//@ Configure instance 2 creating an Admin User.
+var cnfPath2 = testutil.getSandboxConfPath(__mysql_sandbox_port2);
+dba.configureLocalInstance(__sandbox_uri2, {mycnfPath: cnfPath2, clusterAdmin: "gr_user", clusterAdminPassword: "gr_pass"});
+
+//@ Configure instance 3 creating an Admin User.
+var cnfPath3 = testutil.getSandboxConfPath(__mysql_sandbox_port3);
+dba.configureLocalInstance(__sandbox_uri3, {mycnfPath: cnfPath3, clusterAdmin: "gr_user", clusterAdminPassword: "gr_pass"});
+
+//@ Create a cluster with 3 members.
+var admin_user_uri1 = "gr_user:gr_pass@localhost:"+__mysql_sandbox_port1;
+var admin_user_uri2 = "gr_user:gr_pass@localhost:"+__mysql_sandbox_port2;
+var admin_user_uri3 = "gr_user:gr_pass@localhost:"+__mysql_sandbox_port3;
+shell.connect(admin_user_uri1);
+var cluster = dba.createCluster("test_cluster");
+
+cluster.addInstance(admin_user_uri2);
+testutil.waitMemberState(__mysql_sandbox_port2, "ONLINE");
+
+cluster.addInstance(admin_user_uri3);
+testutil.waitMemberState(__mysql_sandbox_port3, "ONLINE");
+
+//@<OUT> Show cluster status, all online.
+cluster.status();
+
+//@ Kill instance 2
+testutil.killSandbox(__mysql_sandbox_port2);
+// Since the cluster has quorum, the instance will be kicked off the
+// Cluster going OFFLINE->UNREACHABLE->(MISSING)
+testutil.waitMemberState(__mysql_sandbox_port2, "(MISSING)");
+
+//@ Kill instance 3
+testutil.killSandbox(__mysql_sandbox_port3);
+// Waiting for the third added instance to become unreachable
+// Will remain unreachable since there's no quorum to kick it off
+testutil.waitMemberState(__mysql_sandbox_port3, "UNREACHABLE");
+
+//@ Cluster.forceQuorumUsingPartitionOf success
+cluster.forceQuorumUsingPartitionOf(admin_user_uri1);
+
+//@<OUT> Show cluster status.
+cluster.status();
+
+// Clean-up deployed instances.
+session.close();
+cluster.disconnect();
+testutil.destroySandbox(__mysql_sandbox_port1);
+testutil.destroySandbox(__mysql_sandbox_port2, true);
+testutil.destroySandbox(__mysql_sandbox_port3, true);
---
+++ unittest/scripts/auto/js_adminapi/validation/bug27089930_forcequorum_non_root_user.js
@@ -0,0 +1,92 @@
+//@ Configure instance 1 creating an Admin User.
+||
+
+//@ Configure instance 2 creating an Admin User.
+||
+
+//@ Configure instance 3 creating an Admin User.
+||
+
+//@ Create a cluster with 3 members.
+||
+
+//@<OUT> Show cluster status, all online.
+{
+    "clusterName": "test_cluster",
+    "defaultReplicaSet": {
+        "name": "default",
+        "primary": "localhost:<<<__mysql_sandbox_port1>>>",
+        "ssl": "REQUIRED",
+        "status": "OK",
+        "statusText": "Cluster is ONLINE and can tolerate up to ONE failure.",
+        "topology": {
+            "localhost:<<<__mysql_sandbox_port1>>>": {
+                "address": "localhost:<<<__mysql_sandbox_port1>>>",
+                "mode": "R/W",
+                "readReplicas": {},
+                "role": "HA",
+                "status": "ONLINE"
+            },
+            "localhost:<<<__mysql_sandbox_port2>>>": {
+                "address": "localhost:<<<__mysql_sandbox_port2>>>",
+                "mode": "R/O",
+                "readReplicas": {},
+                "role": "HA",
+                "status": "ONLINE"
+            },
+            "localhost:<<<__mysql_sandbox_port3>>>": {
+                "address": "localhost:<<<__mysql_sandbox_port3>>>",
+                "mode": "R/O",
+                "readReplicas": {},
+                "role": "HA",
+                "status": "ONLINE"
+            }
+        }
+    },
+    "groupInformationSourceMember": "mysql://gr_user@localhost:<<<__mysql_sandbox_port1>>>"
+}
+
+//@ Kill instance 2
+||
+
+//@ Kill instance 3
+||
+
+//@ Cluster.forceQuorumUsingPartitionOf success
+||
+
+//@<OUT> Show cluster status.
+{
+    "clusterName": "test_cluster",
+    "defaultReplicaSet": {
+        "name": "default",
+        "primary": "localhost:<<<__mysql_sandbox_port1>>>",
+        "ssl": "REQUIRED",
+        "status": "OK_NO_TOLERANCE",
+        "statusText": "Cluster is NOT tolerant to any failures. 2 members are not active",
+        "topology": {
+            "localhost:<<<__mysql_sandbox_port1>>>": {
+                "address": "localhost:<<<__mysql_sandbox_port1>>>",
+                "mode": "R/W",
+                "readReplicas": {},
+                "role": "HA",
+                "status": "ONLINE"
+            },
+            "localhost:<<<__mysql_sandbox_port2>>>": {
+                "address": "localhost:<<<__mysql_sandbox_port2>>>",
+                "mode": "R/O",
+                "readReplicas": {},
+                "role": "HA",
+                "status": "(MISSING)"
+            },
+            "localhost:<<<__mysql_sandbox_port3>>>": {
+                "address": "localhost:<<<__mysql_sandbox_port3>>>",
+                "mode": "R/O",
+                "readReplicas": {},
+                "role": "HA",
+                "status": "(MISSING)"
+            }
+        }
+    },
+    "groupInformationSourceMember": "mysql://gr_user@localhost:<<<__mysql_sandbox_port1>>>"
+}
