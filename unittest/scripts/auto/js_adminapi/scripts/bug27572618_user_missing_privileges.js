0a746da55bc85ad1176a776d20cca66ff389474a
Paulo Jesus <paulo.jesus@oracle.com>
2018-02-28 11:25:57 +0000

    BUG#27572618 : MySQL Shell creates a root user missing privilege on SYS
    The removeInstance() command was issuing an error reporting missing
    privileges when using a (admin) user created by dba.configureLocalInstance().
    In more detail, the error indicated that the execute command was denied to
    the user for routine 'sys.version_major'.
    
    This issue was already fixed by the patch for BUG#26986141, which removed the
    use of the sys.version_major(), sys.version_minor(), and sys.version_patch()
    to obtain the server version information, no longer requiring EXECUTE
    privileges on the "sys" schema.
    
    This patch adds a test for regression.

---
+++ unittest/scripts/auto/js_adminapi/scripts/bug27572618_user_missing_privileges.js
@@ -0,0 +1,27 @@
+// Deploy Instances
+testutil.deploySandbox(__mysql_sandbox_port1, "root");
+testutil.deploySandbox(__mysql_sandbox_port2, "root");
+
+//@ Configure instance 1 creating an Admin User.
+var cnfPath1 = testutil.getSandboxConfPath(__mysql_sandbox_port1);
+dba.configureLocalInstance(__sandbox_uri1, {mycnfPath: cnfPath1, clusterAdmin: "gr_user", clusterAdminPassword: "gr_pass"});
+
+//@ Configure instance 2 creating an Admin User.
+var cnfPath2 = testutil.getSandboxConfPath(__mysql_sandbox_port2);
+dba.configureLocalInstance(__sandbox_uri2, {mycnfPath: cnfPath2, clusterAdmin: "gr_user", clusterAdminPassword: "gr_pass"});
+
+//@ Create cluster.
+shell.connect("gr_user:gr_pass@localhost:"+__mysql_sandbox_port1);
+var cluster = dba.createCluster("test_cluster");
+
+cluster.addInstance("gr_user:gr_pass@localhost:"+__mysql_sandbox_port2);
+testutil.waitMemberState(__mysql_sandbox_port2, "ONLINE");
+
+//@ Remove one instance using Admin user (cannot fail with lack of privileges).
+cluster.removeInstance("gr_user:gr_pass@localhost:"+__mysql_sandbox_port2);
+
+// Clean-up deployed instances.
+session.close();
+cluster.disconnect();
+testutil.destroySandbox(__mysql_sandbox_port1);
+testutil.destroySandbox(__mysql_sandbox_port2);
