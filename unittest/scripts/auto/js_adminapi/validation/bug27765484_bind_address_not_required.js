1ad4b22b402311bdde99ebbbd4e423ae3fd49200
Miguel Araujo <miguel.araujo@oracle.com>
2018-04-03 16:18:32 +0100

    BUG#27765484 : BIND_ADDRESS IN MY.CNF NOT FIXED BY CHECKINSTANCECONFIGURATION/CONFIGUREINSTANCE
    It has been defined in the past that one of the requirements for InnoDB
    Cluster is that 'bind_address' must be unset or set to the default
    value, thus
    dba.checkInstanceConfiguration() reports an invalid instance
    configuration if
    'bind_address' is set to a value not the default one on the targed
    instance.
    In top of that, dba.createCluster(), since it validates the instance
    configuration before proceeding with the operation, fails with an error
    indicating the instance configuration is invalid.
    
    However, such requirement is too strict and is actually a recommendation
    and not a requirement from GR so the decision is to remove it.
    
    This patch removes the requirement from the checks above mentioned.
    
    Added tests to meet the changes.

---
+++ unittest/scripts/auto/js_adminapi/validation/bug27765484_bind_address_not_required.js
@@ -0,0 +1,18 @@
+//@<OUT> checkInstanceConfiguration with bind_address set no error
+The instance 'localhost:<<<__mysql_sandbox_port1>>>' is valid for InnoDB cluster usage.
+
+{
+    "status": "ok"
+}
+
+//@<OUT> createCluster from instance with bind_address set
+Validating instance at localhost:<<<__mysql_sandbox_port1>>>...
+Instance detected as a sandbox.
+Please note that sandbox instances are only suitable for deploying test clusters for use within the same host.
+
+This instance reports its own address as <<<hostname>>>
+
+Instance configuration is suitable.
+Creating InnoDB cluster 'testCluster' on 'root@localhost:<<<__mysql_sandbox_port1>>>'...
+<<<(__version_num<80005) ? "WARNING: On instance 'localhost:"+__mysql_sandbox_port1+"' membership change cannot be persisted since MySQL version "+__version+" does not support the SET PERSIST command (MySQL version >= 8.0.5 required). Please use the <Dba>.configureLocalInstance command locally to persist the changes.\n":""\>>>
+<Cluster:testCluster>
