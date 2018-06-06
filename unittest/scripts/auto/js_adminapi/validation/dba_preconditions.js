dddcbd0e78f848c2624c5eefe2c4eeec4a7b4eb4
Paulo Jesus <paulo.jesus@oracle.com>
2018-06-06 19:16:30 +0100

    BUG#27508627 rebootClusterFromCompleteOutage should not point to force quorum
    The rebootClusterFromCompleteOutage() operation suggest the use of the
    forceQuorumUsingPartitionOf() operation when trying to execute the operation
    on an instance that already belongs to a InnoDB Cluster and that is reachable,
    independently if there is quorum or not. The indication to use
    forceQuorumUsingPartitionOf() in the error message is misleading and should
    be removed, especially if there is quorum.
    
    This patch fixes this issue by introducing an additional quorum verification
    to the existing validation and only adding the indication to use
    forceQuorumUsingPartitionOf() in the error message if quorum was in fact lost
    according to the checked instance.
    
    Test was added for regression.

--- unittest/scripts/auto/js_adminapi/validation/dba_preconditions.js
+++ unittest/scripts/auto/js_adminapi/validation/dba_preconditions.js
@@ -49,7 +49,7 @@
 ||Dba.dropMetadataSchema: No operation executed, use the 'force' option
 
 //@# Dba_preconditions_innodb, reboot_cluster_from_complete_outage_fails
-||Dba.rebootClusterFromCompleteOutage: The MySQL instance '<<<real_hostname>>>:<<<__mysql_sandbox_port1>>>' belongs to an InnoDB Cluster and is reachable. Please use <Cluster>.forceQuorumUsingPartitionOf() to restore from the quorum loss.
+||Dba.rebootClusterFromCompleteOutage: The MySQL instance '<<<real_hostname>>>:<<<__mysql_sandbox_port1>>>' belongs to an InnoDB Cluster and is reachable.
 
 //@ Cleanup
 ||
