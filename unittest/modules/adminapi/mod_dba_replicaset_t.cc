d4c40fce1f32e6daf0fc8d9dd216db503b730ebd
Miguel Araujo <miguel.araujo@oracle.com>
2018-06-21 18:45:55 +0100

    BUG#28219398 INSTANCE ADDED TO CLUSTER STUCK IN RECOVERY PHASE DUE TO WRONG CHANGE MASTER CMD
    Occasionally, when adding an instance to an existing cluster, the instance
    gets stuck in the distributed-recovery phase resulting in an immutable
    reported status of 'RECOVERING'.
    This issue happens every-time the generated password for the replication-user
    contains the following sequence of characters: '%s'.
    
    The issue is caused by an internal mechanism to replace the occurrences of
    '%s' by '?' in any executed query. This leads to the 'CHANGE MASTER' command
    issued to initiate the distributed recovery to have a different value for the
    password field of the replication-user, causing a connection error.
    
    This patch fixed the issue by removing the internal mechanism to replace the
    occurrences of '%' by '?' in the executed queries and by updating all internal
    queries that use '%s' as the wildcard for pattern substitution to use '?'.
    
    Added new unit-test for regression.
    
    Change-Id: I1c40882dbf2e7729918e745438955118e1b8b9a8

--- unittest/modules/adminapi/mod_dba_replicaset_t.cc
+++ unittest/modules/adminapi/mod_dba_replicaset_t.cc
@@ -23,10 +23,12 @@
 #include "modules/adminapi/mod_dba_common.h"
 #include "modules/adminapi/mod_dba_metadata_storage.h"
 #include "modules/adminapi/mod_dba_replicaset.h"
+#include "modules/adminapi/mod_dba_sql.h"
 #include "modules/mod_shell.h"
 #include "mysqlshdk/libs/db/connection_options.h"
 #include "mysqlshdk/libs/db/mysql/session.h"
 #include "unittest/test_utils/admin_api_test.h"
+#include "unittest/test_utils/mod_testutils.h"
 
 using mysqlshdk::mysql::Instance;
 using mysqlshdk::mysql::Var_qualifier;
@@ -47,6 +49,31 @@ class Dba_replicaset_test : public Admin_api_test {
   virtual void SetUp() {
     Admin_api_test::SetUp();
     reset_replayable_shell();
+
+    execute("shell.connect('root:root@localhost:" +
+            std::to_string(_mysql_sandbox_port1) + "')");
+
+    auto dba = _interactive_shell->shell_context()
+                   ->get_global("dba")
+                   .as_object<mysqlsh::dba::Dba>();
+    if (!dba) {
+      auto idba = _interactive_shell->shell_context()
+                      ->get_global("dba")
+                      .as_object<shcore::Global_dba>();
+      dba = std::dynamic_pointer_cast<mysqlsh::dba::Dba>(idba->get_target());
+    }
+
+    shcore::Argument_list args;
+    args.emplace_back("sample");
+    m_cluster = dba->get_cluster_(args).as_object<mysqlsh::dba::Cluster>();
+
+    execute("session.close()");
+  }
+
+  virtual void TearDown() {
+    shcore::Argument_list args;
+    m_cluster->disconnect(args);
+    Admin_api_test::TearDown();
   }
 
   static void SetUpTestCase() {
@@ -72,6 +99,8 @@ class Dba_replicaset_test : public Admin_api_test {
 
     return session;
   }
+
+  std::shared_ptr<mysqlsh::dba::Cluster> m_cluster;
 };
 
 // If the information on the Metadata and the GR group
@@ -190,4 +219,70 @@ TEST_F(Dba_replicaset_test, rescan_cluster_with_new_instances) {
   md_session->close();
 }
 
+// Regression test for BUG#28219398
+// INSTANCE ADDED TO CLUSTER STUCK IN RECOVERY PHASE DUE TO WRONG CHANGE MASTER
+// CMD:
+//
+// Occasionally, when adding an instance to an existing cluster, the instance
+// gets stuck in the distributed-recovery phase resulting in an immutable
+// reported status of 'RECOVERY'.
+//
+// The issue happens every-time the generated password for the replication-user
+// contains the following sequence of characters: '%s'.
+TEST_F(Dba_replicaset_test, bug28219398) {
+  std::string replication_user, replication_pwd;
+  mysqlshdk::db::Connection_options connection_options;
+
+  replication_user = "mysql_innodb_cluster_r1711111111";
+
+  // The password must contain the sequence of chars '%s';
+  replication_pwd = "|t+-*2]1@+T?e%&H;*.%s)>)F/U}9,$?";
+
+  // Create the replication_user on the cluster
+  auto md_session = create_session(_mysql_sandbox_port1);
+
+  md_session->query("CREATE USER IF NOT EXISTS '" + replication_user +
+                    "'@'localhost' IDENTIFIED BY /*(*/ '" + replication_pwd +
+                    "' /*)*/ ");
+
+  md_session->query("GRANT REPLICATION SLAVE ON *.* to /*(*/ '" +
+                    replication_user + "'@'localhost' /*)*/");
+
+  // Set the connection options for _mysql_sandbox_port3
+  connection_options.set_host("localhost");
+  connection_options.set_port(_mysql_sandbox_port3);
+  connection_options.set_user("root");
+  connection_options.set_password("root");
+
+  try {
+    m_cluster->get_default_replicaset()->add_instance(
+        connection_options, {}, replication_user, replication_pwd, false, "",
+        true, false);
+
+    // Set the Shell global session
+    execute("shell.connect('root:root@localhost:" +
+            std::to_string(_mysql_sandbox_port3) + "')");
+
+    testutil->wait_member_state(_mysql_sandbox_port3, "ONLINE", false);
+
+    execute("session.close()");
+
+    auto session = create_session(_mysql_sandbox_port3);
+    auto instance_type = mysqlsh::dba::get_gr_instance_type(session);
+
+    mysqlsh::dba::Cluster_check_info state =
+        mysqlsh::dba::get_replication_group_state(session, instance_type);
+
+    session->close();
+
+    // The instance should be 'ONLINE' and not stuck in 'RECOVERING'
+    EXPECT_EQ(state.source_state, mysqlsh::dba::ManagedInstance::OnlineRO);
+  } catch (const shcore::Exception &e) {
+    SCOPED_TRACE(e.what());
+    SCOPED_TRACE("Unexpected failure adding instance.");
+    ADD_FAILURE();
+  }
+
+  md_session->close();
+}
 }  // namespace tests
