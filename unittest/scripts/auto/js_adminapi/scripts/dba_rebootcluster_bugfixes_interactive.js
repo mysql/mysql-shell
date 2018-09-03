f77910278235f5f649e6b1dc211ccd6520fab5af
Miguel Araujo <miguel.araujo@oracle.com>
2018-09-03 17:05:09 +0100

    Renaming AdminAPI automatic test file to follow the standard naming:
    <object>_<function>_bugfixes.js
    
    Change-Id: I2ce5cac9b0a0f2e7ba461b7f83eca326c5fdc79c

--- unittest/scripts/auto/js_adminapi/scripts/dba_rebootcluster_bugfixes_interactive.js
+++ unittest/scripts/auto/js_adminapi/scripts/dba_rebootcluster_bugfixes_interactive.js
@@ -3,9 +3,26 @@ testutil.deploySandbox(__mysql_sandbox_port1, "root", {loose_group_replication_e
 testutil.deploySandbox(__mysql_sandbox_port2, "root", {loose_group_replication_exit_state_action: "READ_ONLY"});
 testutil.deploySandbox(__mysql_sandbox_port3, "root", {loose_group_replication_exit_state_action: "READ_ONLY"});
 
+// BUG#28207565: DBA.REBOOTCLUSTERFROMCOMPLETEOUTAGE DOES NOT USE DEFAULT
+// CLUSTER
+//
+// On the default interactive mode, whenever using the function
+// dba.rebootClusterFromCompleteOutage without any parameter, the function
+// should assume the cluster to be rebooted is the default one but it
+// doesn't and fails with an error specifying the cluster name '' does not
+// exist.
+//
+// The issue is caused by the handling of the interactive layer on which if
+// the user wants to rejoin the instances detected back to the cluster, or
+// remove from the metadata, a dictionary of options is added to the
+// function
+// callback causing on a failure on the detection of the default cluster
+// which is based on the call of the function without any parameters.
+
 //@ Deploy cluster
 shell.connect(__sandbox_uri1);
 var c = dba.createCluster("myCluster", {memberSslMode: 'DISABLED'});
+testutil.waitMemberState(__mysql_sandbox_port1, "ONLINE");
 c.addInstance(__sandbox_uri2);
 testutil.waitMemberState(__mysql_sandbox_port2, "ONLINE");
 c.addInstance(__sandbox_uri3);
@@ -24,12 +41,12 @@ session.close();
 testutil.killSandbox(__mysql_sandbox_port1);
 
 //@ Start the members again
+testutil.startSandbox(__mysql_sandbox_port3);
+testutil.waitForDelayedGRStart(__mysql_sandbox_port3, 'root', 0);
 testutil.startSandbox(__mysql_sandbox_port2);
 testutil.waitForDelayedGRStart(__mysql_sandbox_port2, 'root', 0);
 testutil.startSandbox(__mysql_sandbox_port1);
-testutil.startSandbox(__mysql_sandbox_port3);
 testutil.waitForDelayedGRStart(__mysql_sandbox_port1, 'root', 0);
-testutil.waitForDelayedGRStart(__mysql_sandbox_port3, 'root', 0);
 
 // Reboot cluster from complete outage, not specifying the name
 
@@ -38,7 +55,9 @@ testutil.expectPrompt("Would you like to rejoin it to the cluster? [y/N]: ", "y"
 testutil.expectPrompt("Would you like to rejoin it to the cluster? [y/N]: ", "y");
 
 //@ Reboot cluster from complete outage, not specifying the name
-c = dba.rebootClusterFromCompleteOutage();
+var c = dba.rebootClusterFromCompleteOutage();
+
+// Waiting for the instances to become online
 testutil.waitMemberState(__mysql_sandbox_port1, "ONLINE");
 testutil.waitMemberState(__mysql_sandbox_port2, "ONLINE");
 testutil.waitMemberState(__mysql_sandbox_port3, "ONLINE");
