dea0b2dd8a581d0e8d7807df87e8f74664792eb3
Paweł Andruszkiewicz <pawel.andruszkiewicz@oracle.com>
2018-12-07 12:31:37 +0100

    Fixes for Windows platform
    With the addition of a new method Cluster.options() there is an
    ambiguity between that method and shell.options property. When
    registering a help entry, if parent topic is specified, but was not yet
    registered, the new topic is added to an orphans list. Later on, when
    parent topic is registered, all matching orphans are added as well.
    Since orphans are identified using the parent name, 'options' can be
    matched to either the new method or existing property.
    
    This fix ensures that orphans cannot be registered to a topic which
    cannot have children topics (i.e. a method).
    
    Change-Id: I80643db7f4f77de69fe306b5caea4a446ab4bfa9

--- mysqlshdk/shellcore/utils_help.cc
+++ mysqlshdk/shellcore/utils_help.cc
@@ -380,7 +380,8 @@ void Help_registry::register_topic(Help_topic *topic, bool new_topic,
 
   // If there are orphans associated to the new topic, they are properly
   // registered
-  if (m_orphans.find(topic->m_help_tag) != m_orphans.end()) {
+  if (!topic->is_member() &&
+      m_orphans.find(topic->m_help_tag) != m_orphans.end()) {
     auto orphans = m_orphans[topic->m_help_tag];
     m_orphans.erase(topic->m_help_tag);
 
