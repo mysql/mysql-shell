2af1638d936c765bc2b0d5c2b83642ea7a58daf9
Alfredo Kojima <alfredo.kengi.kojima@oracle.com>
2020-08-24 07:00:52 -0700

    Fix compilation error caused by unused macro
    Change-Id: I73d3d64c5eb7c66227c2e9e3eb6930557af94832

--- modules/util/dump/dump_writer.h
+++ modules/util/dump/dump_writer.h
@@ -34,13 +34,6 @@
 #include "mysqlshdk/libs/db/row.h"
 #include "mysqlshdk/libs/storage/ifile.h"
 
-#if !defined(__GNUC__) || (__GNUC__ == 2 && __GNUC_MINOR__ < 96)
-#define __builtin_expect(x, expected_value) (x)
-#endif
-
-#define likely(x) __builtin_expect((x), 1)
-#define unlikely(x) __builtin_expect((x), 0)
-
 namespace mysqlsh {
 namespace dump {
 
