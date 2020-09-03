01d9fa39c52bc1b5c9359cbdd62eea9cbda5e235
Marcin Szalowicz <marcin.szalowicz@oracle.com>
2020-09-03 21:26:13 +0200

    BUG#31517184 CRASH TRYING TO IMPORTTABLE A COMPRESSED FILE
    Use only one worker when importing a compressed file
    as we can't chunk/split compressed files.
    
    Change-Id: I2c534c0d2152d43266fcb60f59b0a37f2c41725b

--- mysqlshdk/libs/storage/compressed_file.h
+++ mysqlshdk/libs/storage/compressed_file.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2019, 2020, Oracle and/or its affiliates.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License, version 2.0,
@@ -63,6 +63,7 @@ class Compressed_file : public IFile {
   void remove() override;
 
   IFile *file() const { return m_file.get(); }
+  bool is_compressed() const override { return true; }
 
  private:
   std::unique_ptr<IFile> m_file;
