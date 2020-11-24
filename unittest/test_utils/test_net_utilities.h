bda53adffb5718832d6d8b62fca2e505c837ba7a
Rene Ramirez <j.rene.ramirez@oracle.com>
2020-11-24 03:07:26 -0600

    Updating clang-format to version 10
    - Updated the clang-format configuration file to match the server one.
    - Updated formatting on shell code base
    
    Change-Id: I8683a86d563546ca66657e0c1f037b81a802fe6e

--- unittest/test_utils/test_net_utilities.h
+++ unittest/test_utils/test_net_utilities.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2019, 2020, Oracle and/or its affiliates.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License, version 2.0,
@@ -194,8 +194,8 @@ class Test_net_utilities : public mysqlshdk::utils::Net {
 
   std::string get_hostname_impl() const override { return m_hostname; }
 
-  std::string get_fqdn_impl(const std::string &address) const
-      noexcept override {
+  std::string get_fqdn_impl(
+      const std::string &address) const noexcept override {
     const std::string key = "get_fqdn:" + address;
     switch (m_mode) {
       case mysqlshdk::db::replay::Mode::Record: {
