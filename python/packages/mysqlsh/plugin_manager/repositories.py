5ab5848e082094e2d2fff3f9b4ed972d89b89157
Rene Ramirez <j.rene.ramirez@oracle.com>
2021-03-23 06:57:53 -0600

    Fixing Packaging of OSX Python Deps
    Change-Id: Iac4c010776b9d2e9e487c228911f93cab673ed2f

--- python/packages/mysqlsh/plugin_manager/repositories.py
+++ python/packages/mysqlsh/plugin_manager/repositories.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2020, Oracle and/or its affiliates.
+# Copyright (c) 2020, 2021, Oracle and/or its affiliates.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License, version 2.0,
@@ -37,7 +37,7 @@ DEFAULT_PLUGIN_REPOSITORIES = [{
     "url":
     "https://cdn.mysql.com/windows/installer/manifest.zip",
     "manifestPath":
-    "AdditionalPayload\\mysql-shell-plugins-manifest.json"
+    "AdditionalPayload/mysql-shell-plugins-manifest.json"
 }]
 
 
