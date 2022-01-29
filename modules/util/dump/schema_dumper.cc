a208221e763adf5b2ded6577d4264e46e8f55951
Krzysztof Grzadziel <krzysztof.grzadziel@oracle.com>
2022-01-29 11:25:53 +0100

    Rename CHARSET_INFO.name to m_coll_name
    Change introduced by MySQL Server BUG#33773026
    
    For better code readability/maintanability:
    
      - Rename CHARSET_INFO.name to m_coll_name
    
      - Rename the function get_charset_name() to get_collation_name().
    
    Change-Id: I6e864b209e52177bc99275dc00c9efcb08d85e7c

--- modules/util/dump/schema_dumper.cc
+++ modules/util/dump/schema_dumper.cc
@@ -1,26 +1,25 @@
 /*
-   Copyright (c) 2000, 2021, Oracle and/or its affiliates.
-
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License, version 2.0,
-   as published by the Free Software Foundation.
-
-   This program is also distributed with certain software (including
-   but not limited to OpenSSL) that is licensed under separate terms,
-   as designated in a particular file or component or in included license
-   documentation.  The authors of MySQL hereby grant you an additional
-   permission to link the program and your derivative works with the
-   separately licensed software that they have included with MySQL.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License, version 2.0, for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA
-*/
+ * Copyright (c) 2000, 2022, Oracle and/or its affiliates.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2.0,
+ * as published by the Free Software Foundation.
+ *
+ * This program is also distributed with certain software (including
+ * but not limited to OpenSSL) that is licensed under separate terms, as
+ * designated in a particular file or component or in included license
+ * documentation.  The authors of MySQL hereby grant you an additional
+ * permission to link the program and your derivative works with the
+ * separately licensed software that they have included with MySQL.
+ * This program is distributed in the hope that it will be useful,  but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+ * the GNU General Public License, version 2.0, for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
 
 // Dump a table's contents and format to a text file.
 // Adapted from mysqldump.cc
@@ -161,7 +160,7 @@ void switch_db_collation(IFile *sql_file, const std::string &db_name,
 
     fprintf(sql_file, "ALTER DATABASE %s CHARACTER SET %s COLLATE %s %s\n",
             quoted_db_name.c_str(), (const char *)db_cl->csname,
-            (const char *)db_cl->name, (const char *)delimiter);
+            (const char *)db_cl->m_coll_name, (const char *)delimiter);
 
     *db_cl_altered = 1;
     return;
@@ -181,7 +180,7 @@ void restore_db_collation(IFile *sql_file, const std::string &db_name,
 
   fprintf(sql_file, "ALTER DATABASE %s CHARACTER SET %s COLLATE %s %s\n",
           quoted_db_name.c_str(), (const char *)db_cl->csname,
-          (const char *)db_cl->name, (const char *)delimiter);
+          (const char *)db_cl->m_coll_name, (const char *)delimiter);
 }
 
 void switch_cs_variables(IFile *sql_file, const char *delimiter,
