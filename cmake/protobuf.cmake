42d7ca5f12793e8ec4c71e147fae3992939f0b71
Paweł Andruszkiewicz <pawel.andruszkiewicz@oracle.com>
2022-02-15 11:26:05 +0100

    Bump protobuf to version 3.19.4
    Change-Id: Ic034eeef5a5fc7df99147e88d68b5bafbef37ca0

--- cmake/protobuf.cmake
+++ cmake/protobuf.cmake
@@ -1,4 +1,4 @@
-# Copyright (c) 2015, 2020, Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2015, 2022, Oracle and/or its affiliates.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License, version 2.0,
@@ -26,7 +26,7 @@
 # default locations will be searched.
 #
 
-SET(PROTOBUF_VERSION "3.11.4")
+SET(PROTOBUF_VERSION "3.19.4")
 SET(WITH_PROTOBUF $ENV{WITH_PROTOBUF} CACHE PATH "Protobuf location")
 
 IF(WITH_PROTOBUF)
