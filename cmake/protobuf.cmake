931ee77927c03b1b1a9669cb2836fb850cd9b097
Paweł Andruszkiewicz <pawel.andruszkiewicz@oracle.com>
2024-01-15 12:51:58 +0100

    Bundled protobuf has been updated to 4.24.4.
    Change-Id: Iebd3e41707a1ebc704c31955fe66c291bfa6fc66
    (cherry picked from commit c3113247818defdf0d69cddf84ac1d8f687a557a)

--- cmake/protobuf.cmake
+++ cmake/protobuf.cmake
@@ -1,4 +1,4 @@
-# Copyright (c) 2015, 2022, Oracle and/or its affiliates.
+# Copyright (c) 2015, 2024, Oracle and/or its affiliates.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License, version 2.0,
@@ -26,7 +26,7 @@
 # default locations will be searched.
 #
 
-SET(PROTOBUF_VERSION "3.19.4")
+SET(PROTOBUF_VERSION "4.24.4")
 SET(WITH_PROTOBUF $ENV{WITH_PROTOBUF} CACHE PATH "Protobuf location")
 
 IF(WITH_PROTOBUF)
@@ -86,27 +86,38 @@ IF(WITH_PROTOBUF)
 ELSE()
   IF (MYSQL_SOURCE_DIR AND MYSQL_BUILD_DIR)
 
+    SET(_protobuf_protoc_dir "${MYSQL_BUILD_DIR}/runtime_output_directory")
+
+    IF(WIN32)
+      STRING(APPEND _protobuf_protoc_dir "/${CMAKE_BUILD_TYPE}")
+    ENDIF()
+
     FIND_PROGRAM(PROTOBUF_PROTOC_EXECUTABLE
       NAMES protoc
       DOC "The Google Protocol Buffers Compiler"
-      PATHS ${MYSQL_BUILD_DIR}/bin
+      PATHS "${_protobuf_protoc_dir}/bin"
       NO_DEFAULT_PATH
     )
 
-    SET(PROTOBUF_INCLUDE_DIR "${MYSQL_SOURCE_DIR}/extra/protobuf/protobuf-${PROTOBUF_VERSION}/src")
+    string(REGEX MATCH "^([0-9]+)\\.([0-9]+)\\.([0-9]+)" PROTOBUF_VERSION_MATCH "${PROTOBUF_VERSION}")
+    SET(PROTOBUF_VERSION_MAJOR "${CMAKE_MATCH_1}")
+    SET(PROTOBUF_VERSION_MINOR "${CMAKE_MATCH_2}")
+    SET(PROTOBUF_VERSION_PATCH "${CMAKE_MATCH_3}")
+
+    SET(PROTOBUF_BUNDLED_VERSION "${PROTOBUF_VERSION_MINOR}.${PROTOBUF_VERSION_PATCH}")
+
+    SET(PROTOBUF_INCLUDE_DIR "${MYSQL_SOURCE_DIR}/extra/protobuf/protobuf-${PROTOBUF_BUNDLED_VERSION}/src")
     IF (WIN32)
       IF(CMAKE_BUILD_TYPE STREQUAL Debug)
-        SET(PROTOBUF_LIBRARY "${MYSQL_BUILD_DIR}/extra/protobuf/protobuf-${PROTOBUF_VERSION}/cmake/${CMAKE_BUILD_TYPE}/libprotobufd.lib")
-        SET(PROTOBUF_LIBRARY_DEBUG "${MYSQL_BUILD_DIR}/extra/protobuf/protobuf-${PROTOBUF_VERSION}/cmake/${CMAKE_BUILD_TYPE}/libprotobufd.lib")
+        SET(PROTOBUF_LIBRARY "${MYSQL_BUILD_DIR}/extra/protobuf/protobuf-${PROTOBUF_BUNDLED_VERSION}/${CMAKE_BUILD_TYPE}/libprotobufd.lib")
       ELSE()
-        SET(PROTOBUF_LIBRARY "${MYSQL_BUILD_DIR}/extra/protobuf/protobuf-${PROTOBUF_VERSION}/cmake/${CMAKE_BUILD_TYPE}/libprotobuf.lib")
+        SET(PROTOBUF_LIBRARY "${MYSQL_BUILD_DIR}/extra/protobuf/protobuf-${PROTOBUF_BUNDLED_VERSION}/${CMAKE_BUILD_TYPE}/libprotobuf.lib")
       ENDIF()
     ELSE()
       IF(CMAKE_BUILD_TYPE STREQUAL Debug)
-        SET(PROTOBUF_LIBRARY "${MYSQL_BUILD_DIR}/extra/protobuf/protobuf-${PROTOBUF_VERSION}/cmake/libprotobufd.a")
-        SET(PROTOBUF_LIBRARY_DEBUG "${MYSQL_BUILD_DIR}/extra/protobuf/protobuf-${PROTOBUF_VERSION}/cmake/libprotobufd.a")
+        SET(PROTOBUF_LIBRARY "${MYSQL_BUILD_DIR}/extra/protobuf/protobuf-${PROTOBUF_BUNDLED_VERSION}/libprotobufd.a")
       ELSE()
-        SET(PROTOBUF_LIBRARY "${MYSQL_BUILD_DIR}/extra/protobuf/protobuf-${PROTOBUF_VERSION}/cmake/libprotobuf.a")
+        SET(PROTOBUF_LIBRARY "${MYSQL_BUILD_DIR}/extra/protobuf/protobuf-${PROTOBUF_BUNDLED_VERSION}/libprotobuf.a")
       ENDIF()
     ENDIF()
   ENDIF()
@@ -118,6 +129,34 @@ if (NOT ONLY_PROTOBUF_VERSION)
 
   IF(NOT PROTOBUF_FOUND)
     MESSAGE(FATAL_ERROR "Protobuf could not be found")
+  ENDIF()
+
+  IF(NOT WITH_PROTOBUF)
+    SET(ABSEIL_DIR "extra/abseil/abseil-cpp-20230802.1")
+    list(APPEND PROTOBUF_INCLUDE_DIRS "${MYSQL_SOURCE_DIR}/${ABSEIL_DIR}")
+
+    IF(WIN32)
+      list(APPEND PROTOBUF_LIBRARIES "${MYSQL_BUILD_DIR}/${ABSEIL_DIR}/absl/${CMAKE_BUILD_TYPE}/abseil_dll.lib")
+      list(APPEND PROTOBUF_LIBRARIES "${MYSQL_BUILD_DIR}/extra/protobuf/protobuf-${PROTOBUF_BUNDLED_VERSION}/third_party/utf8_range/${CMAKE_BUILD_TYPE}/utf8_validity.lib")
+      SET(BUNDLED_ABSEIL_LIBRARY "${MYSQL_BUILD_DIR}/library_output_directory/${CMAKE_BUILD_TYPE}/abseil_dll.dll")
+    ELSE()
+      FILE(GLOB_RECURSE _absl_libs LIST_DIRECTORIES false "${MYSQL_BUILD_DIR}/${ABSEIL_DIR}/absl/*.a")
+
+      IF(NOT APPLE)
+        list(APPEND PROTOBUF_LIBRARIES "-Wl,--start-group")
+      ENDIF()
+
+      list(APPEND PROTOBUF_LIBRARIES ${_absl_libs})
+      list(APPEND PROTOBUF_LIBRARIES "${MYSQL_BUILD_DIR}/extra/protobuf/protobuf-${PROTOBUF_BUNDLED_VERSION}/third_party/utf8_range/libutf8_validity.a")
+
+      IF(NOT APPLE)
+        list(APPEND PROTOBUF_LIBRARIES "-Wl,--end-group")
+      ENDIF()
+
+      IF(APPLE)
+        list(APPEND PROTOBUF_LIBRARIES "-Wl,-framework,CoreFoundation")
+      ENDIF()
+    ENDIF()
   ENDIF()
 
   MESSAGE("PROTOBUF_INCLUDE_DIRS: ${PROTOBUF_INCLUDE_DIRS}")
