d2eb61f257caae5399fbdf2d1928356c6f7a44e3
Paweł Andruszkiewicz <pawel.andruszkiewicz@oracle.com>
2024-01-24 16:36:19 +0100

    BUG#36221818 Copy operation hangs
    In certain situations, copy operation would hang when target was
    constantly scanning for metadata, while source was already waiting to
    write the data files. This could happen if chunking tasks were quick to
    be executed and produced data tasks, while metadata tasks are still
    being queued.
    
    As a fix, metadata tasks are queued first, while chunking tasks are
    gathered in a container, and pushed into queue only after the metadata
    task are ready.
    
    Change-Id: I2e02f065b273036b4ba47af59a6dce834b0198ea

--- modules/util/dump/dumper.cc
+++ modules/util/dump/dumper.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, 2023, Oracle and/or its affiliates.
+ * Copyright (c) 2020, 2024, Oracle and/or its affiliates.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License, version 2.0,
@@ -29,6 +29,7 @@
 #include <functional>
 #include <iterator>
 #include <limits>
+#include <list>
 #include <set>
 #include <type_traits>
 #include <utility>
@@ -3191,6 +3192,8 @@ void Dumper::create_table_tasks() {
 
   const auto console = current_console();
 
+  std::list<Table_task> pending_tasks;
+
   for (const auto &schema : m_schema_infos) {
     for (const auto &table : schema.tables) {
       auto task = create_table_task(schema, table);
@@ -3210,15 +3213,19 @@ void Dumper::create_table_tasks() {
                                      task.index.info, task.extra_filter);
       }
 
-      push_table_task(std::move(task));
+      pending_tasks.emplace_back(std::move(task));
     }
 
     // BUG#34663934 - allow exporting data from views
     if (m_options.is_export_only()) {
       for (const auto &view : schema.views) {
-        push_table_task(create_table_task(schema, view));
+        pending_tasks.emplace_back(create_table_task(schema, view));
       }
     }
+  }
+
+  for (auto &task : pending_tasks) {
+    push_table_task(std::move(task));
   }
 
   m_all_table_metadata_tasks_scheduled = true;
