799ab38c916129890c39a8079e2daaceeaf7b256
Rene Ramirez <j.rene.ramirez@oracle.com>
2024-05-02 09:34:12 -0600

    Fixing tests with left over schemas.
    Change-Id: I437c9d1773e060bf7dc1a62b49c510a9028d5bea

--- unittest/scripts/py_devapi/scripts/mysql_session.py
+++ unittest/scripts/py_devapi/scripts/mysql_session.py
@@ -22,72 +22,73 @@ validate_members(classicSession, [
   '_get_socket_fd'])
 
 #@ ClassicSession: accessing Schemas
-schemas = classicSession.get_schemas();
+schemas = classicSession.get_schemas()
 
 #@ ClassicSession: accessing individual schema
-schema = classicSession.get_schema('mysql');
+schema = classicSession.get_schema('mysql')
 
 #@ ClassicSession: accessing default schema
-dschema = classicSession.get_default_schema();
+dschema = classicSession.get_default_schema()
 
 #@ ClassicSession: accessing current schema
-cschema = classicSession.get_current_schema();
+cschema = classicSession.get_current_schema()
 
 #@ ClassicSession: create schema
-sf = classicSession.create_schema('classic_session_schema');
+sf = classicSession.create_schema('classic_session_schema')
 
 #@ ClassicSession: set current schema
-classicSession.set_current_schema('classic_session_schema');
+classicSession.set_current_schema('classic_session_schema')
 
 #@ ClassicSession: drop schema
-classicSession.drop_schema('node_session_schema');
+classicSession.drop_schema('node_session_schema')
 
 #@Preparation for transaction tests
-result = classicSession.run_sql('drop schema if exists classic_session_schema');
-result = classicSession.run_sql('create schema classic_session_schema');
-result = classicSession.run_sql('use classic_session_schema');
+result = classicSession.run_sql('drop schema if exists classic_session_schema')
+result = classicSession.run_sql('create schema classic_session_schema')
+result = classicSession.run_sql('use classic_session_schema')
 
 #@ ClassicSession: Transaction handling: rollback
-result = classicSession.run_sql('create table sample (name varchar(50) primary key)');
-classicSession.start_transaction();
-res1 = classicSession.run_sql('insert into sample values ("john")');
-res2 = classicSession.run_sql('insert into sample values ("carol")');
-res3 = classicSession.run_sql('insert into sample values ("jack")');
-classicSession.rollback();
+result = classicSession.run_sql('create table sample (name varchar(50) primary key)')
+classicSession.start_transaction()
+res1 = classicSession.run_sql('insert into sample values ("john")')
+res2 = classicSession.run_sql('insert into sample values ("carol")')
+res3 = classicSession.run_sql('insert into sample values ("jack")')
+classicSession.rollback()
 
-result = classicSession.run_sql('select * from sample');
-print('Inserted Documents:', len(result.fetch_all()));
+result = classicSession.run_sql('select * from sample')
+print('Inserted Documents:', len(result.fetch_all()))
 
 #@ ClassicSession: Transaction handling: commit
 //! [ClassicSession: SQL execution example]
 # Begins a transaction
-classicSession.start_transaction();
+classicSession.start_transaction()
 
 # Inserts some records
-res1 = classicSession.run_sql('insert into sample values ("john")');
-res2 = classicSession.run_sql('insert into sample values ("carol")');
-res3 = classicSession.run_sql('insert into sample values ("jack")');
+res1 = classicSession.run_sql('insert into sample values ("john")')
+res2 = classicSession.run_sql('insert into sample values ("carol")')
+res3 = classicSession.run_sql('insert into sample values ("jack")')
 
 # Commits the transaction
-classicSession.commit();
+classicSession.commit()
 //! [ClassicSession: SQL execution example]
 
-result = classicSession.run_sql('select * from sample');
-print('Inserted Documents:', len(result.fetch_all()));
+result = classicSession.run_sql('select * from sample')
+print('Inserted Documents:', len(result.fetch_all()))
 
 #@ ClassicSession: date handling
-classicSession.run_sql("select cast('9999-12-31 23:59:59.999999' as datetime(6))");
+classicSession.run_sql("select cast('9999-12-31 23:59:59.999999' as datetime(6))")
 
 #@# ClassicSession: run_sql errors
-classicSession.run_sql();
-classicSession.run_sql(1, 2, 3);
-classicSession.run_sql(1);
-classicSession.run_sql('select ?', 5);
-classicSession.run_sql('select ?, ?', [1, 2, 3]);
-classicSession.run_sql('select ?, ?', [1]);
+classicSession.run_sql()
+classicSession.run_sql(1, 2, 3)
+classicSession.run_sql(1)
+classicSession.run_sql('select ?', 5)
+classicSession.run_sql('select ?, ?', [1, 2, 3])
+classicSession.run_sql('select ?, ?', [1])
 
 #@<OUT> ClassicSession: run_sql placeholders
-classicSession.run_sql("select ?, ?", ['hello', 1234]);
+classicSession.run_sql("select ?, ?", ['hello', 1234])
 
 # Cleanup
-classicSession.close();
+classicSession.run_sql('drop schema if exists classic_session_schema')
+classicSession.close()
