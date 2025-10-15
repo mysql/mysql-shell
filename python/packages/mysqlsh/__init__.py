426183be1f2327599de7d5f2eca24ebf2efaa488
Alfredo Kojima <alfredo.kengi.kojima@oracle.com>
2025-10-15 11:32:22 -0700

    Add type annotations to mysqlsh.DBError and Error
    Change-Id: Ie7a6d698730c3eac8e657ff18e52f1c21d544642

--- python/packages/mysqlsh/__init__.py
+++ python/packages/mysqlsh/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2020, 2024, Oracle and/or its affiliates.
+# Copyright (c) 2020, 2025, Oracle and/or its affiliates.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License, version 2.0,
@@ -22,12 +22,16 @@
 # 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 
 class Error(Exception):
-    def __init__(self, code, msg=None):
+    code: int | None = None
+    msg: str | None = None
+
+    def __init__(self, code: int, msg: str | None = None):
         if msg is None:
-            msg = code
-            code = None
-        self.code = code
-        self.msg = msg
+            self.msg = str(code)
+            self.code = None
+        else:
+            self.code = code
+            self.msg = msg
         self.args = (code, msg)
 
     def __str__(self):
@@ -39,7 +43,7 @@ class Error(Exception):
 
 
 class DBError(Error):
-    def __init__(self, code, msg, sqlstate=None):
+    def __init__(self, code: int, msg: str, sqlstate=None):
         super().__init__(code, msg)
         self.sqlstate = sqlstate
 
